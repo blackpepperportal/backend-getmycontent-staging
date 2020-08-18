d19d554edcbd74e8b1567bd9dd3b3c198079753b
vithyaCodegama <vithya@codegama.com>
2020-08-18 12:45:33 +0530

    Added is_published column to posts

--- database/migrations/2020_08_16_051012_create_posts_table.php
+++ database/migrations/2020_08_16_051012_create_posts_table.php
@@ -21,6 +21,7 @@ class CreatePostsTable extends Migration
             $table->dateTime('publish_time')->nullable();
             $table->float('amount')->default(0.00);
             $table->tinyInteger('is_paid_post')->default(0);
+            $table->tinyInteger('is_published')->default(1);
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
