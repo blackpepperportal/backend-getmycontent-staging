5aead8a904a57dfc87f39f72ba0342117f89ddf1
bhawyacodegama <bhawya@codegama.com>
2020-09-11 12:23:11 +0530

    Migration Changes

---
+++ database/migrations/2020_09_11_064910_create_followers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->comment("login user id - content_creators,users");
+            $table->integer('follower_id')->comment("fallowers id of content_creators");
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
