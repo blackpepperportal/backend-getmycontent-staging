eea622c21af9faca737c47b850699a478dcb986c
benodarry <beno@codegama.com>
2020-09-18 00:03:52 +0530

    ui fixes done

--- resources/views/admin/delivery_address/_search.blade.php
+++ resources/views/admin/delivery_address/_search.blade.php
@@ -2,9 +2,9 @@
 
     <div class="row">
 
-        <div class="col-6"></div>
+        <div class="col-xs-12 col-sm-12 col-lg-6 col-md-12"></div>
 
-        <div class="col-6">
+        <div class="col-xs-12 col-sm-12 col-lg-6 col-md-12">
 
             <div class="input-group">
                
