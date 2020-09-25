b20a777fe8538a446542bedab029d9bb09769ecc
Arun <arun@codegama.com>
2020-09-25 14:09:15 +0530

    Subcription auto renewal changes

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -10,8 +10,36 @@ use App\Helpers\Helper;
 
 use App\StaticPage;
 
+use App\SubscriptionPayment;
+
+use App\User;
+
+use App\Subscription;
+
+use App\Repositories\PaymentRepository as PaymentRepo;
+
 class ApplicationController extends Controller
 {
+
+    protected $loginUser;
+
+    protected $skip, $take;
+
+    public function __construct(Request $request) {
+
+        Log::info(url()->current());
+
+        Log::info("Request Data".print_r($request->all(), true));
+        
+        $this->loginUser = User::find($request->id);
+
+        $this->skip = $request->skip ?: 0;
+
+        $this->take = $request->take ?: (Setting::get('admin_take_count') ?: TAKE_COUNT);
+
+        $this->timezone = $this->loginUser->timezone ?? "America/New_York";
+
+    }
 
     /**
      * @method static_pages_api()
@@ -81,22 +109,21 @@ class ApplicationController extends Controller
 
 
 
-public function subscriptions_autorenewal(Request $request){
+public function subscription_payments_autorenewal(Request $request){
 
         try {
             $current_timestamp = \Carbon\Carbon::now()->toDateTimeString();
-            $subscriptionDetails = SubscriptionPayment::where('is_current_subscription',1)->where('expiry_date','<', $current_timestamp)->get();
+            $subscription_payment_details = SubscriptionPayment::where('is_current_subscription',1)->where('expiry_date','<', $current_timestamp)->get();
 
-            if(!$subscriptionDetails) {
+            if(!$subscription_payment_details) {
 
                 throw new Exception(api_error(129), 129);
 
             }
 
-            foreach ($subscriptionDetails as $subscription_details){
+            foreach ($subscription_payment_details as $subscription_payment_detail){
 
-                $user_details = User::where('id',  $subscription_details->user_id)->first();
-
+                $user_details = User::where('id',  $subscription_payment_detail->user_id)->first();
                 if ($user_details->one_time_subscription == 1){
                     DB::beginTransaction();
 
@@ -108,14 +135,13 @@ public function subscriptions_autorenewal(Request $request){
 
                     $custom_errors = ['subscription_id' => api_error(129)];
 
-                    Helper::custom_validator($subscription_details->all(), $rules, $custom_errors);
+                    Helper::custom_validator($subscription_payment_detail->toArray(), $rules, $custom_errors);
 
                     // Validation end
 
                     // Check the subscription is available
 
-                    $subscription_details = Subscription::Approved()->firstWhere('id',  $subscription_details->subscription_id);
-
+                    $subscription_details = Subscription::Approved()->firstWhere('id',  $subscription_payment_detail->subscription_id);
                     $is_user_subscribed_free_plan = $this->loginUser->one_time_subscription ?? NO;
 
                     if($subscription_details->amount <= 0 && $is_user_subscribed_free_plan) {
@@ -124,7 +150,8 @@ public function subscriptions_autorenewal(Request $request){
 
                     }
 
-                    $request->request->add(['payment_mode' => CARD]);
+                $payment_details['payment_mode'] = CARD;
+
 
                     $total = $user_pay_amount = $subscription_details->amount ?? 0.00;
 
@@ -138,15 +165,12 @@ public function subscriptions_autorenewal(Request $request){
 
                         }
 
-                        $request->request->add([
-                            'total' => $total,
-                            'customer_id' => $card_details->customer_id,
-                            'user_pay_amount' => $user_pay_amount,
-                            'paid_amount' => $user_pay_amount,
-                        ]);
+                        $payment_details['total'] = $total;
+                        $payment_details['customer_id'] = $card_details->customer_id;
+                        $payment_details['user_pay_amount'] = $user_pay_amount;
+                        $payment_details['paid_amount'] = $user_pay_amount;
 
-
-                        $card_payment_response = PaymentRepo::subscriptions_payment_by_stripe($request, $subscription_details)->getData();
+                     $card_payment_response = PaymentRepo::subscriptions_payment_by_stripe($request, $subscription_details)->getData();
 
                         if($card_payment_response->success == false) {
 
@@ -156,11 +180,13 @@ public function subscriptions_autorenewal(Request $request){
 
                         $card_payment_data = $card_payment_response->data;
 
-                        $request->request->add(['paid_amount' => $card_payment_data->paid_amount, 'payment_id' => $card_payment_data->payment_id, 'paid_status' => $card_payment_data->paid_status]);
+                        $payment_details['paid_amount'] = $card_payment_data->paid_amount;
+                        $payment_details['payment_id'] = $card_payment_data->payment_id;
+                        $payment_details['paid_status'] = $card_payment_data->paid_status;
 
                     }
 
-                    $payment_response = PaymentRepo::subscriptions_payment_save($request, $subscription_details)->getData();
+                    $payment_response = PaymentRepo::subscriptions_payment_save($payment_details, $subscription_details)->getData();
 
                     if($payment_response->success) {
                         $data['subscription_id']=$subscription_details->subscription_id;
@@ -199,6 +225,6 @@ public function subscriptions_autorenewal(Request $request){
         }
 
     }
-    
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -138,7 +138,7 @@ Route::group(['prefix' => 'user' , 'middleware' => 'cors'], function() {
 
         Route::post('subscriptions_autorenewal_status','Api\SubscriptionApiController@subscriptions_autorenewal_status');
 
-        Route::post('subscriptions_autorenewal','ApplicationController@subscriptions_autorenewal');
+        Route::post('subscription_payments_autorenewal','ApplicationController@subscription_payments_autorenewal');
 
     });
 
