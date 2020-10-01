b5006f2adae1855c7129cb678d1f2d074a45f285
benodarry <beno@codegama.com>
2020-10-01 22:58:24 +0530

    ui fixes done

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -45,7 +45,7 @@
 
                             <div class="row">
 
-                                <div class="col-xs-12 col-sm-12 col-lg-3 col-md-6 resp-mrg-btm-md">
+                                <div class="col-xs-12 col-sm-12 col-lg-2 col-md-6 resp-mrg-btm-md">
                                     @if(Request::has('search_key'))
                                     <p class="text-muted">Search results for <b>{{Request::get('search_key')}}</b></p>
                                     @endif
@@ -68,10 +68,10 @@
                                     </select>
 
                                 </div>
+                    
+                                <div class="col-xs-12 col-sm-12 col-lg-6 mx-auto col-md-12">
 
-                                <div class="col-xs-12 col-sm-12 col-lg-6 col-md-12">
-
-                                    <div class="input-group">
+                                    <div class="input-group form-margin-left-sm">
 
                                         <input type="text" class="form-control" name="search_key" value="{{Request::get('search_key')??''}}" placeholder="{{tr('users_search_placeholder')}}"> <span class="input-group-btn">
                                             &nbsp
