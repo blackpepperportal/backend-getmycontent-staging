a2bc12f30d37fa111bbc25cdf05f4c0609199947
vithyaCodegama <vithya@codegama.com>
2020-11-06 12:57:36 +0530

    Changes

--- app/Http/Controllers/Api/FollowersApiController.php
+++ app/Http/Controllers/Api/FollowersApiController.php
@@ -52,7 +52,7 @@ class FollowersApiController extends Controller
 
         try {
 
-            $following_user_ids = Follower::where('follower_id', $request->id)->pluck('user_id');
+            $following_user_ids = Follower::where('follower_id', $request->id)->pluck('user_id')->toArray();
 
             array_push($following_user_ids, $request->id);
 
