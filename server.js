4d0db20d2a5f67108e288665dddaf226b7698746
vithyaCodegama <vithya@codegama.com>
2020-11-16 22:26:35 +0530

    Issue Fixes

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ io.on('connection', function (socket) {
 
         var sent_status = socket.broadcast.to(receiver).emit('message', data);
 
-        url = chat_save_url+'api/user/chat_messages/save?sender_user_id='+data.sender_user_id
+        url = chat_save_url+'api/user/chat_messages/save?from_user_id='+data.from_user_id
         +'&to_user_id='+data.to_user_id
         +'&message='+data.message;
 
