e9f2a13ebc82230687a3841fef3482e8002f12c4
vithyaCodegama <vithya@codegama.com>
2020-11-20 15:07:18 +0530

    Chnges

--- database/seeds/PageDemoSeeder.php
+++ database/seeds/PageDemoSeeder.php
@@ -11,17 +11,17 @@ class PageDemoSeeder extends Seeder
      */
     public function run()
     {
-        if(Schema::hasTable('pages')) {
+        if(Schema::hasTable('static_pages')) {
 
         	$static_pages = json_decode(json_encode(['about' , 'contact' , 'privacy' , 'terms' , 'help']));
 
         	foreach ($static_pages as $key => $value) {
 
-    			$page_details = DB::table('pages')->where('type' ,$value)->count();
+    			$page_details = DB::table('static_pages')->where('type' ,$value)->count();
 
     			if(!$page_details) {
 
-    				DB::table('pages')->insert([
+    				DB::table('static_pages')->insert([
     	         		[
     				        'unique_id' => $value,
                             'heading' => $value,
