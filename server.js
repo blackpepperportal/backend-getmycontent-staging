cd8863e69fd6bdc45be7f20130d0f9f1236bd4a1
vithyaCodegama <vithya@codegama.com>
2020-11-23 12:27:44 +0530

    Changes

--- server.js
+++ server.js
@@ -70,7 +70,15 @@ io.on('connection', function (socket) {
 
         console.log("send message Start");
 
-        var receiver = "user_id_"+data.user_id+"_to_user_id_"+data.to_user_id;
+        if(data.loggedin_user_id == data.from_user_id) {
+
+            var receiver = "user_id_"+data.to_user_id+"_to_user_id_"+data.from_user_id;
+
+        } else {
+            
+            var receiver = "user_id_"+data.from_user_id+"_to_user_id_"+data.to_user_id;
+        }
+
 
         console.log('data', data);
 
