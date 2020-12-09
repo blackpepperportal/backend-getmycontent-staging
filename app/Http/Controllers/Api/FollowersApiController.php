35d1acd764a89d1754092bd97983470a1eb4f834
codegama <ganesh@codegama.com>
2020-12-09 22:01:16 +0530

    changes

--- app/Http/Controllers/Api/FollowersApiController.php
+++ app/Http/Controllers/Api/FollowersApiController.php
@@ -295,7 +295,9 @@ class FollowersApiController extends Controller
 
         try {
 
-            $base_query = $total_query = Follower::CommonResponse()->where('user_id', $request->id);
+            $blocked_users = blocked_users($request->id);
+
+            $base_query = $total_query = Follower::CommonResponse()->whereNotIn('follower_id',$blocked_users)->where('user_id', $request->id);
 
             $followers = $base_query->skip($this->skip)->take($this->take)->orderBy('followers.created_at', 'desc')->get();
 
@@ -348,7 +350,9 @@ class FollowersApiController extends Controller
 
         try {
 
-            $base_query = $total_query = Follower::CommonResponse()->where('follower_id', $request->id);
+            $blocked_users = blocked_users($request->id);
+
+            $base_query = $total_query = Follower::CommonResponse()->whereNotIn('user_id',$blocked_users)->where('follower_id', $request->id);
 
             $followers = $base_query->skip($this->skip)->take($this->take)->orderBy('followers.created_at', 'desc')->get();
 
