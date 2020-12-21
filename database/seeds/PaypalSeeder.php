8fb4fc71d79db168283d141fa7ca547614370bac
bhawyacodegama <bhawya@codegama.com>
2020-12-21 23:57:40 +0530

    changes

--- database/seeds/PaypalSeeder.php
+++ database/seeds/PaypalSeeder.php
@@ -15,6 +15,18 @@ class PaypalSeeder extends Seeder
     		[
 		        'key' => 'is_paypal_enabled',
 		        'value' => 0
+		    ],
+		    [
+		        'key' => 'PAYPAL_ID',
+		        'value' => 'AaXkweZD5g9s0X3BsO0Y4Q-kNzbmLZaog0mbmVGrTT5IX0O73LoLVcHp17e6pkG7Vm04JEUuG6up30LD'
+		    ],
+		    [
+		        'key' => 'PAYPAL_SECRET',
+		        'value' => ''
+		    ],
+		    [
+		        'key' => 'PAYPAL_MODE',
+		        'value' => 'sandbox'
 		    ]
 		]);
     }
