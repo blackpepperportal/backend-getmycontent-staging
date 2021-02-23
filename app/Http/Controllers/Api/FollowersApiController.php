50315521b4edabf4d9fbbc2e5cff3aa088fc41e7
bhawyacodegama <bhawya@codegama.com>
2021-02-23 12:57:04 +0530

    Changes

--- app/Http/Controllers/Api/FollowersApiController.php
+++ app/Http/Controllers/Api/FollowersApiController.php
@@ -456,7 +456,8 @@ class FollowersApiController extends Controller
 
             $chat_message = \App\ChatMessage::where('chat_messages.to_user_id', $request->from_user_id)->where('status', NO)->update(['status' => YES]);
 
-            $chat_messages = $base_query->skip($this->skip)->take($this->take)->get();
+            $chat_messages = $base_query->skip($this->skip)->take($this->take)
+                    ->orderBy('chat_messages.id', 'asc')->get();
 
             foreach ($chat_messages as $key => $value) {
                 
