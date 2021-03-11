d6613fd15aa629981fd8cc6f1bb78d6e4e21ff86
bhawyacodegama <bhawya@codegama.com>
2021-03-11 23:00:37 +0530

    ChangeS

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -268,6 +268,16 @@ class ApplicationController extends Controller
             
             $chat_user->save();
 
+            $chat_to_user = \App\ChatUser::where('from_user_id', $request->to_user_id)->where('to_user_id', $request->from_user_id)->first() ?? new \App\ChatUser();
+
+            $chat_to_user->from_user_id = $request->to_user_id;
+
+            $chat_to_user->to_user_id = $request->from_user_id;
+                
+            $chat_user->update_at = date('Y-m-d H:i:s');
+            
+            $chat_to_user->save();
+
             // $from_chat_user = \App\ChatUser::updateOrCreate($from_chat_user_inputs);
 
             // $to_chat_user_inputs = ['from_user_id' => $request->to_user_id, 'to_user_id' => $request->from_user_id];
@@ -300,16 +310,6 @@ class ApplicationController extends Controller
 
             $chat_message->save();
 
-            $chat_to_user = \App\ChatUser::where('from_user_id', $request->to_user_id)->where('to_user_id', $request->from_user_id)->first() ?? new \App\ChatUser();
-
-            $chat_to_user->from_user_id = $request->to_user_id;
-
-            $chat_to_user->to_user_id = $request->from_user_id;
-                
-            $chat_user->update_at = $chat_message->update_at;
-            
-            $chat_to_user->save();
-            
             DB::commit();
 
             return $this->sendResponse("", "", $chat_message);
